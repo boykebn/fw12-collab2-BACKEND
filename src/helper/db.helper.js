43ed1715a74d30da02a16cf4e256680ef3a05520
Putut Nardianto L <pututnardiantol@gmail.com>
2022-12-30 22:22:33 +0700

    fix typo

--- src/helper/db.helper.js
+++ src/helper/db.helper.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-const { Pool } = require("pg");
-
-require("dotenv").config();
-
-const dbHelper = new Pool({
-  connectionString: `${process.env.DB_URL}`,
-});
-
-dbHelper.connect((err) =>{
-  if(err) {
-    console.log(err)
-    console.log('database is not connect')
-  } else{
-    console.log('database is connect!')
-  }
-})
-
-module.exports = dbHelper;
-=======
 const { Pool } = require('pg')
 
 const db = new Pool({
@@ -25,5 +5,4 @@ const db = new Pool({
     process.env.DATABASE_URL 
 })
 
-mocule.exports = db
->>>>>>> 84078145ac80e92c5df875c9c09290bfa831d247
+module.exports = db

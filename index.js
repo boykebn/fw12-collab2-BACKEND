6e5682b88d1d0d08e5ea723ec38968b7957cf7b0
Boyke <boykeberryn@gmail.com>
2022-12-30 13:51:19 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config({
+  path: ".env",
+});
+
+const app = express();
+const port = process.env.PORT || 8888 ;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// app.use("/", require("./src/routes"));
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "berhasil",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});

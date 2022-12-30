043e89748c9b49bea8b5d1209540524107fce0a9
Cupu-Boy <fajrir357@gmail.com>
2022-12-30 22:02:12 +0700

    push helper and middleware

---
+++ src/middleware/upload.middleware.js
@@ -0,0 +1,42 @@
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, require('path').join(process.cwd(), 'assets/uploads'))
+    },
+    filename: (req, file, callback) => {
+        const getExtension = file.originalname.split('.');
+        const extension = getExtension[getExtension.lenght - 1]
+        const name = `${new Date().getDate()}_${new Date().getTime()}.${extension}`
+        callback(null, name)
+    }
+})
+
+const upload = multer({
+    storage,
+    limits: { fileSize: 1000000},
+    fileFilter: (req, file, callback) => {
+        const format = ['jpg', 'png', 'jpeg']
+        const extension = file.originalname.split('.')
+        const cekFormatFile = format.includes(extension[extension.length-1])
+        if(!cekFormatFile){
+            return callback(new Error('Format picture not valid'))
+        } else {
+            return callback(null, true)
+        }
+    }
+})
+
+const uploadMiddleware = upload.single(picture)
+
+module.exports = (req, res, next) => {
+    uploadMiddleware(req, res, (error) => {
+        if(error){
+            return res.status(400).json({
+                success: false,
+                message: error.message
+            })
+        }
+        next()
+    }) 
+}

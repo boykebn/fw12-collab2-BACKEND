d79201cf2ccdf396dc93b47736157fdfa3f732e6
Cupu-Boy <fajrir357@gmail.com>
2022-12-31 18:46:19 +0700

    add CRUD delivery tie

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ routers.use("/product", require("./product.router"));
 routers.use("/size", require("./size.router"));
 routers.use("/category", require("./category.router"));
 routers.use("/productCategory", require("./productCategory.router"));
+routers.use("/deliveryTime", require("./deliveryTime.router"));
 
 module.exports = routers

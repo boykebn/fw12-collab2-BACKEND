4b9f0a5a370ca6d7410f9621055fde4ee757287f
Putut Nardianto L <pututnardiantol@gmail.com>
2022-12-31 22:02:49 +0700

    fix conflict

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,6 @@ routers.use("/category", require("./category.router"));
 routers.use("/productCategory", require("./productCategory.router"));
 routers.use("/deliveryTime", require("./deliveryTime.router"));
 routers.use("/status", require("./status.router"));
-<<<<<<< HEAD
 routers.use("/users", require("./users.router"));
 
 module.exports = routers
-=======
-routers.use("/users", require("./users.router"))
-module.exports = routers;
->>>>>>> 66e9dd1698e7eb353b14f3700da5c058e327f342

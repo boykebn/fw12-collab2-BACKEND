82da7242fa088d0e22c62ca6b3632f3c709ac93e
Cupu-Boy <fajrir357@gmail.com>
2022-12-31 10:07:44 +0700

    add CRUD category

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,6 @@ const routers = require("express").Router();
 
 routers.use("/product", require("./product.router"));
 routers.use("/size", require("./size.router"));
+routers.use("/category", require("./category.router"));
 
 module.exports = routers

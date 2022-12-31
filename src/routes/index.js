7009a6983cc207a137b303a9b773dc9741da8522
arsrsyh <gotrany97@gmail.com>
2022-12-31 22:43:14 +0800

    crud users

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,6 @@ routers.use("/size", require("./size.router"));
 routers.use("/category", require("./category.router"));
 routers.use("/productCategory", require("./productCategory.router"));
 routers.use("/deliveryTime", require("./deliveryTime.router"));
-routers.use("/users", require("./users.router"));
+routers.use("/status", require("./status.router"));
 
 module.exports = routers;

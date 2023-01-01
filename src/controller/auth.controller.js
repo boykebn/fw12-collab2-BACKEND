9ff0f3b3a82d2cce05aed8ed039e42f5f63f0d91
Cupu-Boy <fajrir357@gmail.com>
2023-01-01 20:24:11 +0700

    add forgot password and update password

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,25 +1,14 @@
 const errorHandler = require("../helper/errorHandler.helper");
-const {
-  selectUserByEmail,
-  updateUser,
-  createUsers,
-  createUser,
-} = require("../models/users.model");
-// const {
-//   createForgotPassword,
-//   selectUserByEmailAndCode,
-//   deleteForgotPassword,
-// } = require("../models/forgotPasswords.model");
+const { createForgotPassword, readForgotPasswordByEmailAndCode, deleteForgotPassword } = require("../models/forgotPassword");
+const { selectUserByEmail, updateUsers, createUsers } = require("../models/users.model");
+
 const argon = require("argon2");
 const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
   try {
     const user = await selectUserByEmail(req.body.email);
-    const token = jwt.sign(
-      { id: user.id, role: user.role },
-      process.env.SECRET_KEY
-    );
+    const token = jwt.sign({ id: user.id, role: user.role }, process.env.SECRET_KEY);
     if (user.role == 1) {
       if (await argon.verify(user.password, req.body.password)) {
         return res.status(200).json({
@@ -65,10 +54,7 @@ const register = async (req, res) => {
   try {
     req.body.password = await argon.hash(req.body.password);
     const user = await createUsers(req.body);
-    const token = jwt.sign(
-      { id: user.id, role: user.role },
-      process.env.SECRET_KEY
-    );
+    const token = jwt.sign({ id: user.id, role: user.role }, process.env.SECRET_KEY);
     return res.status(200).json({
       success: true,
       message: "Register Success",
@@ -81,67 +67,69 @@ const register = async (req, res) => {
   }
 };
 
-// const forgotPassword = async (req, res) => {
-//   try {
-//     const { email } = req.body;
-//     const users = await selectUserByEmail(email);
-//     if (users) {
-//       const data = {
-//         email,
-//         userId: users.id,
-//         code: Math.ceil(Math.random() * 90000 + 10000),
-//       };
-//       await createForgotPassword(data);
-//       res.status(200).json({
-//         success: true,
-//         message: "Reset password has been requested.",
-//       });
-//     } else {
-//       return res.status(400).json({
-//         success: false,
-//         message: "User not found",
-//       });
-//     }
-//   } catch (error) {
-//     if (error) errorHandler(error, res);
-//   }
-// };
+const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await selectUserByEmail(email);
+    if(user) {
+      const data = {
+        email,
+        userId: user.id,
+        code: Math.ceil(Math.random() * 90000 + 10000),
+      };
+      const requstResetPassword = await createForgotPassword(data);
+      return res.status(200).json({
+        success: true,
+        message: "Reset password has been requested",
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "Request failed, user doesn't exist",
+      });
+    }
+  } catch (error) {
+    return errorHandler(error, res);
+  }
+};
 
-// const resetPassword = async (req, res) => {
-//   try {
-//     const { password, confirmPassword } = req.body;
-//     if (password === confirmPassword) {
-//       const users = await selectUserByEmailAndCode(req.body);
-//       if (users) {
-//         const newPassword = await argon.hash(req.body.password);
-//         const reset = await updateUser(users.userId, { password: newPassword });
-//         if (reset) {
-//           await deleteForgotPassword(users.id);
-//           return res.status(200).json({
-//             success: true,
-//             message: "Password updated, please re-login.",
-//           });
-//         }
-//       } else {
-//         return res.status(400).json({
-//           success: false,
-//           message: "Email or code is not valid",
-//         });
-//       }
-//     } else {
-//       return res.status(400).json({
-//         success: false,
-//         message: "Password and confirm password must be match",
-//       });
-//     }
-//   } catch (error) {
-//     if (error) errorHandler(error, res);
-//   }
-// };
+const resetPassword = async (req, res) => {
+  try {
+    const { password, confirmPassword } = req.body;
+    if (password === confirmPassword) {
+      const resetRequest = await readForgotPasswordByEmailAndCode(req.body)
+      if(resetRequest){
+        if(new Date(resetRequest.createdAt).getTime() + 15 * 60 * 1000 < new Date().getTime()){
+          throw Error('Code Expired')
+        } 
+        
+        const data = {
+          password : argon.hash(password)
+        }
+
+        const user = await updateUsers(data, resetRequest.userId)
+        const forgotPassword = await deleteForgotPassword(resetRequest.id)
+        return res.status(200).json({
+          success: true,
+          message: "Password success updated, please relogin",
+        });
+      } else{
+        throw Error('Request not found')
+      }
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: "Password and confirm password not match",
+      });
+    }
+  } catch (error) {
+    return errorHandler(error, res);
+  }
+};
 
 module.exports = {
   login,
   register,
-  // forgotPassword,
-  // resetPassword,
+  forgotPassword,
+  resetPassword
 };
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -3,13 +3,13 @@ const authRouter = require("express").Router();
 const {
   login,
   register,
-//   resetPassword,
-//   forgotPassword,
+  resetPassword,
+  forgotPassword,
 } = require("../controller/auth.controller");
 
 authRouter.post("/login", login);
 authRouter.post("/register", register);
-// authRouter.post("/forgotPassword", forgotPassword);
-// authRouter.post("/resetPassword", resetPassword);
+authRouter.post("/forgotPassword", forgotPassword);
+authRouter.post("/resetPassword", resetPassword);
 
 module.exports = authRouter;

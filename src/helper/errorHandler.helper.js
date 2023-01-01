598af83440d90cd7d8fc4a1ec87db51665b68d69
Putut Nardianto L <pututnardiantol@gmail.com>
2023-01-01 21:58:30 +0700

    add errorhandler reset password

--- src/helper/errorHandler.helper.js
+++ src/helper/errorHandler.helper.js
@@ -1,12 +1,18 @@
 const errorHandler = (error, res) => {
   console.log(error);
-  if (error.message.includes(' unique constraint "users_email_unique"') || error.message.includes(`"users_email_key"`)) {
+  if (
+    error.message.includes(' unique constraint "users_email_unique"') ||
+    error.message.includes(`"users_email_key"`)
+  ) {
     return res.status(400).json({
       success: false,
       message: "Email already used",
     });
   }
-  if (error.message.includes(' unique constraint "users_phonenumber_unique"')|| error.message.includes(`users_phoneNumber_key`)) {
+  if (
+    error.message.includes(' unique constraint "users_phonenumber_unique"') ||
+    error.message.includes(`users_phoneNumber_key`)
+  ) {
     return res.status(400).json({
       success: false,
       message: "Phone number already used",
@@ -32,11 +38,17 @@ const errorHandler = (error, res) => {
       message: "Email not registered",
     });
   }
+  if (error.message.includes("Request not found")) {
+    return res.status(400).json({
+      success: false,
+      message: "Code or email isn't correct",
+    });
+  }
   return res.status(500).json({
     succes: false,
     message: "Something happend in our backend",
-    error: error
+    error: error,
   });
 };
 
-module.exports = errorHandler
+module.exports = errorHandler;

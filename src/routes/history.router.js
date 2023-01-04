a14f7726b42bda50e3e50d7e120d6b31c32e6a4d
Putut Nardianto L <pututnardiantol@gmail.com>
2023-01-04 20:30:09 +0700

    add enpoint /history/delete

--- src/routes/history.router.js
+++ src/routes/history.router.js
@@ -6,11 +6,15 @@ const {
   updateHistory,
   deleteHistory,
 } = require("../controller/history.controller");
+const {deleteOrderProduct} = require('../controller/orderedProduct.controller')
 
 History.get("/", getAllHistory);
+History.delete("/delete/:id", deleteOrderProduct);
+
+
+
 History.get("/:id", getHistoryById);
 History.post("/", createHistory);
 History.patch("/:id", updateHistory);
-History.delete("/:id", deleteHistory);
 
 module.exports = History;

d15b8ec81915b92ea8860e0c1245f1282c2fed20
Cupu-Boy <fajrir357@gmail.com>
2023-01-04 10:53:45 +0700

    fix problem

--- src/routes/profile.router.js
+++ src/routes/profile.router.js
@@ -5,6 +5,6 @@ const authMiddleware = require('../middleware/auth.middleware')
 const uploadMiddleware = require('../middleware/upload.middleware')
 
 profile.get('/', authMiddleware, readProfile)
-profile.patch('/', authMiddleware, uploadMiddleware, updateProfile)
+profile.post('/', authMiddleware, uploadMiddleware, updateProfile)
 
 module.exports = profile

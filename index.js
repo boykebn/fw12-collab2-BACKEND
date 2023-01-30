3fd13a6f1d9f8ae69e525faf4f4dc7ff26a12c24
Putut Nardianto L <pututnardiantol@gmail.com>
2023-01-30 16:31:14 +0700

    add app.get in index

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ app.use('/uploads', express.static("uploads/"))
 
 app.use("/", require("./src/routes"));
 
+app.get("/", (request, response) => {
+  return response.status(200).json({
+    success: true,
+    message: "Backend is running well",
+  });
+});
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
   console.log(`http://localhost:${port}/`)
